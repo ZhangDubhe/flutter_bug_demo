9e2f1e82ac37a68e635c992cc9272df2319a74db
ZhangDubhe <zht_dubhe@163.com>
2019-12-20 15:55:05 +0800

    update index

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
       routes: {
-        '/': (BuildContext context) {
+        '/index': (BuildContext context) {
           return MyHomePage(title: 'Flutter Demo Home Page');
         }
       },
